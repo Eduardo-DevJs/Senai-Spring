498653ebe8e173a28f22c3e3bb1877dababa59fc
Eduardo-DevJs <dudev2001@gmail.com>
2023-11-20 17:22:17 -0300

    first commit

---
+++ src/test/java/com/trabalhosenai/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhosenai.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
